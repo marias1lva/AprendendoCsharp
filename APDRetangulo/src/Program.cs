09ab8b6b88dcefa90d7c9f9dde3e7908212360eb
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-06 13:01:10 -0300

    Fazendo exercicios sobre classes, objetos e metodos

---
+++ APDRetangulo/src/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace APDRetangulo {
+    public class Program {
+        static void Main(string[] args) {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre com os valores da largura e da altura do retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("ÁREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+        
+    }
+}
---
+++ APDRetangulo/src/Retangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace APDRetangulo {
+    class Retangulo {
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+        public double Perimetro() {
+            return Largura * 2 + Altura * 2;
+        }
+        public double Diagonal() {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+    }
+}
