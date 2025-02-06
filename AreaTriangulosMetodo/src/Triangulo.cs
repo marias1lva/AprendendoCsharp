b3bb25b8290622c874e969edf606f5f81896296e
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-06 09:55:20 -0300

    Aplicando metodos

---
+++ AreaTriangulosMetodo/src/Triangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AreaTriangulosMetodo {
+    class Triangulo {
+        public double A;
+        public double B;
+        public double C;
+
+        //método para calcular a área do triângulo
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
