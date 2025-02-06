49128b49ce7756e5b6d4fd4ea7125f3caf99522c
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-06 14:29:59 -0300

    Fazendo exercicios sobre classes, objetos e metodos

---
+++ NotasAluno/src/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization; 
+
+namespace NotasAluno {
+    class Aluno {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal() {
+            return Nota1 + Nota2 + Nota3;
+        }
+    }
+}
---
+++ NotasAluno/src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace NotasAluno {
+    class Program {
+        static void Main(String[] args) {
+
+            Aluno a = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            a.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            a.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("NOTA FINAL = " + a.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (a.NotaFinal() >= 60.00) {
+                Console.WriteLine("APROVADO");
+            }
+            else {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + (60.00 - a.NotaFinal()).ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+    }
+}
