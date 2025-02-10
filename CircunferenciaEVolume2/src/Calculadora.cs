5cefae305bc96a90de0de47e6b5d8b938705d79c
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-10 08:15:59 -0300

    Aplicando metodos

---
+++ CircunferenciaEVolume2/src/Calculadora.cs
@@ -0,0 +1,13 @@
+﻿namespace CircunferenciaEVolume2 {
+    class Calculadora {
+        public double Pi = 3.14;
+
+        public double Circunferencia(double r) { // r = raio que a função recebe como entrada
+            return 2.0 * Pi * r;
+        }
+
+        public double Volume(double r) {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
---
+++ CircunferenciaEVolume2/src/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using CircunferenciaEVolume2;
+
+namespace CircunferenciaEVolume1 {
+    class Program {
+        static void Main(string[] args) {
+
+            Calculadora calc = new Calculadora();
+
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = calc.Circunferencia(raio);
+            double vol = calc.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + calc.Pi);
+        }
+    }
+}
