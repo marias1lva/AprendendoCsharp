8665ba4b1c4d72c8f6e35a72aa278b963b918c9a
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-10 08:24:31 -0300

    Aplicando metodos estaticos

---
+++ CircunferenciaEVolume3/src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace CircunferenciaEVolume3 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi);
+        }
+    }
+}
