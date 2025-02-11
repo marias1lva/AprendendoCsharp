997d7db1e298692fdfbfde24a1401844b0cf872d
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-11 08:25:18 -0300

    Aplicando metodos estaticos

---
+++ CotacaoDolar/src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace CotacaoDolar {
+    class Program {
+        static void Main(string[] args) {
+            double dolarCotado, quantosDolares, valorEmReais;
+            Console.Write("Qaul é a cotação do dólar? ");
+            dolarCotado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            quantosDolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            valorEmReais = ConversorDeMoeda.DolarParaReal(quantosDolares, dolarCotado);
+            Console.WriteLine("Valor a ser pago em reais = R$" + valorEmReais.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
