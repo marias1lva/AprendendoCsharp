1c8c44f25751a6785ceac4ff1d05053a2f205070
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-14 09:45:59 -0300

    implementando properties

---
+++ ProdutosLoja4/src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProdutosLoja3 {
+    class Program {
+        static void Main(string[] args) {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+        }
+    }
+}
