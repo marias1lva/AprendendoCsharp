a15144f8ab23f778c61c91aa6e98c49e1f044c84
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-17 08:52:52 -0300

    aplicando vetores

---
+++ PrecoMedio/src/Produto.cs
@@ -0,0 +1,7 @@
+﻿namespace PrecoMedio {
+    internal class Produto {
+
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ PrecoMedio/src/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrecoMedio {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Quantos produtos? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[n];
+            for (int i = 0; i < n; i++) {
+                Console.Write("Produto: ");
+                string nome = Console.ReadLine();
+                Console.Write("Preço: R$");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Produto { Nome = nome, Preco = preco };
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++) {
+                soma += vect[i].Preco;
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine();
+            Console.Write("Preço médio = R$" + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
