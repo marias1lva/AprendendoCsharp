1e0d77ff47ee6304ff424bb7a7dea605b240ca6c
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-17 10:11:56 -0300

    aplicando modificador de parametros ref

---
+++ Triplicar/src/Calculadora.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Triplicar {
+    class Calculadora {
+        public static void Triple( ref int x) {
+            x = x * 3;
+        }
+    }
+}
---
+++ Triplicar/src/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Triplicar {
+    class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            Calculadora.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
