bbb5c72e738c5359a20eaf1ea6c2323f97c184e4
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-17 13:38:13 -0300

    aplicando foreach

---
+++ LacoForeach/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LacoForeach {
+    class Program {
+        static void Main() {
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+
+            Console.WriteLine("Sem foreach: ");
+            for (int i = 0; i < vect.Length; i ++){
+                Console.WriteLine(vect[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Com foreach: ");
+            foreach (string obj in vect) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
