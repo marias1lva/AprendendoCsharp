006b1d76d0c1eef8126ef30f4e17021bc2a876ce
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-17 08:26:30 -0300

    aplicando vetores

---
+++ AlturaMedia/src/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace AlturaMedia {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Quantas pessoas serão? ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+            for(int i = 0; i < n; i++) {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0.0;
+            for(int i = 0; i < n; i++) {
+                soma += vect[i];
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine("Altura média = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
