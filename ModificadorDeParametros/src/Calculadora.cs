b1b01fabbdcbcd8f943c0370109e3d5d6d971076
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-17 09:50:58 -0300

    aplicando modificador de parametros params

---
+++ ModificadorDeParametros/src/Calculadora.cs
@@ -0,0 +1,11 @@
+﻿namespace ModificadorDeParametros {
+    class Calculadora {
+        public static int Soma(params int[] numeros) {
+            int soma = 0;
+            for(int i = 0; i < numeros.Length; i++) {
+                soma += numeros[i];
+            }
+            return soma;
+        }
+    }
+}
---
+++ ModificadorDeParametros/src/Program.cs
@@ -0,0 +1,15 @@
+﻿//exemplo
+using System;
+
+namespace ModificadorDeParametros {
+    class Program {
+        static void Main(string[] args) {
+            int s1 = Calculadora.Soma(2, 3);
+            int s2 = Calculadora.Soma(2, 3, 4);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+        }
+    }
+}
