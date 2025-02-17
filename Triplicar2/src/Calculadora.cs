7f9ff92d9ce8321bfdbe7e62a7e71fd9ab9eb635
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-17 10:19:24 -0300

    aplicando modificador de parametros out

---
+++ Triplicar2/src/Calculadora.cs
@@ -0,0 +1,7 @@
+﻿namespace Triplicar2 {
+    class Calculadora {
+        public static void Triple(int origin, out int result) {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Triplicar2/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Triplicar2 {
+    class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            int triple;
+            Calculadora.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
