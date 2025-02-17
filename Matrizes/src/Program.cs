8feb00bc69afcc36d3c5c576e85399e499c1459b
Maria Eduarda <164123811+marias1lva@users.noreply.github.com>
2025-02-17 20:52:05 -0300

    aprendendo matrizes

---
+++ Matrizes/src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Matrizes {
+    class Program {
+        static void Main(string[] args) {
+            double[,] mat = new double[2, 3]; //primeiro linhas e depois colunas: 2 = linhas, 3 = colunas
+
+            Console.WriteLine(mat.Length); //quantidade de elementos
+            Console.WriteLine(mat.Rank); //quantidade de dimensões 
+            Console.WriteLine(mat.GetLength(0)); //quantidade de elementos na dimensão 0 (linhas da matriz)
+            Console.WriteLine(mat.GetLength(1)); //quantidade de elementos na dimensão 1 (colunas da matriz)
+        }
+    }
+}
