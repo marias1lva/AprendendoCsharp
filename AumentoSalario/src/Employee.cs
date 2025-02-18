79db97bb9eddb84529338ded1a08e9fce782d3e1
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-18 08:02:55 -0300

    aplicando listas

---
+++ AumentoSalario/src/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace AumentoSalario {
+    class Employee {
+        // Propriedades do funcionário
+        public int Id { get; set; }  // Identificador único do funcionário
+        public string Name { get; set; }  // Nome do funcionário
+
+        // Propriedade Salary (Salário) com encapsulamento - somente leitura pública
+        public double Salary { get; private set; }
+
+        // Construtor da classe Employee, responsável por inicializar os atributos
+        public Employee(int id, string name, double salary) {
+            Id = id;       // Atribui o ID passado ao atributo Id
+            Name = name;   // Atribui o Nome passado ao atributo Name
+            Salary = salary; // Atribui o Salário passado ao atributo Salary
+        }
+
+        // Método para aumentar o salário com base em uma porcentagem informada
+        public void IncreaseSalary(double percentage) {
+            Salary += Salary * percentage / 100.0; // Calcula o novo salário com base na porcentagem
+        }
+
+        public override string ToString() {
+            return Id 
+                + ", " 
+                + Name 
+                + ", " 
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ AumentoSalario/src/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace AumentoSalario {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("How many exployees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Criação de uma lista para armazenar os objetos do tipo Employee
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Employee #{i}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                // Cria um novo objeto Employee e adiciona à lista
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            // Busca o funcionário na lista usando uma expressão lambda
+            Employee emp = list.Find(x => x.Id == searchId);
+
+            // Verifica se o funcionário foi encontrado
+            if (emp != null) {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                // Chama o método que aumenta o salário do funcionário
+                emp.IncreaseSalary(percentage);
+            }
+            else {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Update list of employees: ");
+
+            // Percorre a lista e imprime os dados dos funcionários atualizados
+            foreach (Employee obj in list) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
