e2866f05210b1e67170abacac9108e2e6c86c0bc
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-18 09:14:34 -0300

    aplicando expressao condicional ternaria

---
+++ ExpressaoCondicionalTernaria/src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExpressaoCondicionalTernaria {
+    class Program {
+        static void Main(string[] args) {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto;
+            desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            Console.WriteLine(desconto);
+        }
+    }
+}
