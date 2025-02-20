c7ddf5fb2623c3d30b635b638706b105775c1fb6
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-20 15:55:58 -0300

    exercicio aplicando exceptions

---
+++ WithdrawLimit/src/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,8 @@
+﻿using System; 
+
+namespace WithdrawLimit.Entities.Exceptions {
+    class DomainException : ApplicationException {
+        public DomainException(string message) : base(message) {
+        }
+    }
+}
