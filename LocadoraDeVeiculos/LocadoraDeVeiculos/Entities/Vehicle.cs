83ea7cb2f2d2b2802293cd00c82f7d53b8f6a2d2
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-21 14:59:21 -0300

    Aprendendo interface

---
+++ LocadoraDeVeiculos/LocadoraDeVeiculos/Entities/Vehicle.cs
@@ -0,0 +1,9 @@
+﻿namespace LocadoraDeVeiculos.Entities {
+    class Vehicle {
+        public string Model { get; set; }
+
+        public Vehicle(string model) {
+            Model = model;
+        }
+    }
+}
