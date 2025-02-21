fead38677038eebe62a8aed0d3fff592c82a9ff1
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-21 08:55:58 -0300

    aplicando uso de files

---
+++ UsandoArquivos2/src/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace UsandoArquivos2 {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\file1.txt";
+            StreamReader sr = null;
+            try {
+                sr = File.OpenText(path); // Abre o arquivo no caminho especificado para leitura de texto.
+                while (!sr.EndOfStream) { // Enquanto não chegar ao fim do arquivo, continua lendo linha por linha.
+                    string line = sr.ReadLine(); // Lê uma linha do arquivo e armazena na variável 'line'.
+                    Console.WriteLine(line);
+                }               
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
