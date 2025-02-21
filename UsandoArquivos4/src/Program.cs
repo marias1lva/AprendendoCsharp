381320fff1cee072d0ceb9b64f8ee0eb87e3a027
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-21 09:18:25 -0300

    aplicando uso de files

---
+++ UsandoArquivos4/src/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace UsandoArquivos4 {
+    class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try {
+                string[] lines = File.ReadAllLines(sourcePath); // Lê todas as linhas do arquivo de origem e armazena em um array de strings
+                using (StreamWriter sw = File.AppendText(targetPath)) { // Abre o arquivo de destino em modo de adição (append). Se não existir, cria o arquivo
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper()); // Escreve a linha convertida para letras maiúsculas no arquivo de destino
+                    }
+                } // O bloco 'using' garante que o StreamWriter será fechado automaticamente após o uso
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
