333f6aa8ba68a13737f3a398a4769ac8b9fc9f03
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-21 08:35:08 -0300

    aplicando uso de files

---
+++ UsandoArquivos/UsandoArquivos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace UsandoArquivos {
+    class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath); // Copia o conteúdo do file1 para o file2
+                string[] lines = File.ReadAllLines(sourcePath); // Lê todas as linhas do file1 e armazena em um vetor de strings
+                foreach (string line in lines) {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
