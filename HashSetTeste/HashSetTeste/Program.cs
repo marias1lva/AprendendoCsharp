3e5e41744fc7ca108c3912650a99332d46af82e7
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-24 14:05:49 -0300

    Aplicando HashSet

---
+++ HashSetTeste/HashSetTeste/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetTeste {
+    class Program {
+        static void Main(string[] args) {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook")); // True
+            Console.WriteLine(set.Contains("Computer")); // False
+
+            foreach (string p in set) {
+                Console.WriteLine(p); // TV, Notebook, Tablet
+            }
+        }
+    }
+}
