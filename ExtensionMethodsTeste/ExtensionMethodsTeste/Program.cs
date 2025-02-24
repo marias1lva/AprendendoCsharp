6cdf0d5e71e55deda02b69282ad74bd5386ad73b
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-24 16:00:06 -0300

    Comecando exercicio de Extension Methods

---
+++ ExtensionMethodsTeste/ExtensionMethodsTeste/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using ExtensionMethodsTeste.Extensions;
+
+public class ProcessFile {
+    public static void Main() {
+        DateTime dt = new DateTime(2018, 11, 26, 8, 10, 45);
+        Console.WriteLine(dt.ElapsedTime);
+    }
+}
