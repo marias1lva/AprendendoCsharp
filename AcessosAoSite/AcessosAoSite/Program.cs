bb91457c1ff2d46491137bfc47967a1e2802444c
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-24 15:03:49 -0300

    Aplicando HashSet

---
+++ AcessosAoSite/AcessosAoSite/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using AcessosAoSite.Entities;
+
+namespace AcessosAoSite {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter a file full path: ");
+            string path = Console.ReadLine();
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string username = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = username, Instant = instant });        
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
