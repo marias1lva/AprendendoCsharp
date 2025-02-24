f22f665eb73d62a78b6b37ffe19234a13d12af6a
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-24 08:25:47 -0300

    Exercicio de interfaces

---
+++ ProcessamentoDeContrato/ProcessamentoDeContrato/Services/IOnlinePaymentService.cs
@@ -0,0 +1,6 @@
+﻿namespace ProcessamentoDeContrato.Services {
+    interface IOnlinePaymentService {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
