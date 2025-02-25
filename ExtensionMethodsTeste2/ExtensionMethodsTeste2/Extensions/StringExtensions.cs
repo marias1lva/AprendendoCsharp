9ad16ee6121580469725339eca2f67479f1aba81
Maria Eduarda Lima da Silva <maria.silva@glok.com.br>
2025-02-25 08:33:29 -0300

    Exercicio aplicando Extension Methods

---
+++ ExtensionMethodsTeste2/ExtensionMethodsTeste2/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System {
+    static class StringExtensions {
+        public static string Cut(this string thisObj, int count) { // Método de extensão que "corta" uma string até um determinado número de caracteres
+            if (thisObj.Length <= count) { // Verifica se o comprimento da string original é menor ou igual ao número de caracteres solicitado
+                return thisObj; // Se for, retorna a string original sem alterações
+            }else {
+                return thisObj.Substring(0, count) + "..."; // Caso contrário, retorna uma substring da string original, cortada até o comprimento especificado
+            }
+        }
+    }
+}
---
+++ ExtensionMethodsTeste2/ExtensionMethodsTeste2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class ProcessFile {
+    public static void Main() {
+        string s1 = "Good morning dear students!"; // Cria uma string com uma mensagem
+
+        // Chama o método de extensão Cut() para cortar a string até 10 caracteres
+        Console.WriteLine(s1.Cut(10)); // O método retorna os primeiros 10 caracteres da string, adicionando "..." no final
+        // Saída esperada: "Good morni..."
+    }
+}
